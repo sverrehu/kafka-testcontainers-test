48c4612db9e41112d97490cffaf44ca418b66e79
Sverre H Huseby <shh@thathost.com>
2020-11-24 20:06:27 +0100

    A tad better test.

--- src/test/java/no/shhsoft/kafka/AbstractKafkaAdminTest.java
+++ src/test/java/no/shhsoft/kafka/AbstractKafkaAdminTest.java
@@ -6,16 +6,24 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.Set;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public abstract class AbstractKafkaAdminTest {
 
+    private static final String TOPIC_NAME = "my-test-topic";
+
     @Test
-    public final void testSomething() {
+    public final void shouldAddAndListTopic() {
         final Admin admin = getAdmin();
-        admin.createTopics(Collections.singleton(new NewTopic("my-test-topic", 1, (short) 1)));
+        admin.createTopics(Collections.singleton(new NewTopic(TOPIC_NAME, 1, (short) 1)));
         final ListTopicsResult topics = admin.listTopics();
         try {
-            System.out.println("Topics: " + topics.names().get().size());
+            final Set<String> topicNames = topics.names().get();
+            assertFalse("List of topic names is empty. Expected at least one.", topicNames.isEmpty());
+            assertTrue("Expected to find topic named " + TOPIC_NAME, topicNames.contains(TOPIC_NAME));
         } catch (final Exception e) {
             throw new RuntimeException(e);
         }
