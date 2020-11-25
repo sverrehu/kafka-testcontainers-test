74117442930d3311fdf437954c772fb3c0be98be
Sverre H. Huseby <sverre.huseby@statnett.no>
2020-11-25 10:25:18 +0100

    Configurable users.

--- src/main/java/no/shhsoft/kafka/AlternativeSaslPlaintextKafkaContainer.java
+++ src/main/java/no/shhsoft/kafka/AlternativeSaslPlaintextKafkaContainer.java
@@ -4,6 +4,8 @@ import org.testcontainers.images.builder.Transferable;
 import org.testcontainers.utility.DockerImageName;
 
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * How SaslPlaintextKafkaContainer would look if KafkaContainer from the
@@ -15,7 +17,12 @@ public final class AlternativeSaslPlaintextKafkaContainer
 extends AlternativeKafkaContainer {
 
     private static final DockerImageName DEFAULT_IMAGE = DockerImageName.parse("confluentinc/cp-kafka").withTag("6.0.0");
+    public static final String DEFAULT_SUPER_USERNAME = "kafka";
+    public static final String DEFAULT_SUPER_PASSWORD = "kafka";
     private static final String JAAS_CONFIG_FILE = "/tmp/broker_jaas.conf";
+    private String superUsername = DEFAULT_SUPER_USERNAME;
+    private String superPassword = DEFAULT_SUPER_PASSWORD;
+    private final Map<String, String> usernamesAndPasswords = new HashMap<>();
 
     public AlternativeSaslPlaintextKafkaContainer() {
         this(DEFAULT_IMAGE);
@@ -29,22 +36,66 @@ extends AlternativeKafkaContainer {
         withEnv("KAFKA_SASL_MECHANISM_INTER_BROKER_PROTOCOL", "PLAIN");
         withEnv("KAFKA_SASL_ENABLED_MECHANISMS", "PLAIN");
         withEnv("KAFKA_AUTHORIZER_CLASS_NAME", "kafka.security.authorizer.AclAuthorizer");
-        withEnv("KAFKA_SUPER_USERS", "User:kafka");
-        withEnv("KAFKA_LISTENER_NAME_SASL_PLAINTEXT_PLAIN_SASL_JAAS_CONFIG",
-                "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"kafka\" password=\"kafka\" user_kafka=\"kafka\" user_alice=\"alice-secret\" user_bob=\"bob-secret\"");
         withEnv("KAFKA_OPTS", "-Djava.security.auth.login.config=" + JAAS_CONFIG_FILE);
+        withSuperUser(superUsername, superPassword);
+        withUser("alice", "alice-secret");
+        withUser("bob", "bob-secret");
+    }
+
+    public AlternativeKafkaContainer withSuperUser(final String username, final String password) {
+        this.superUsername = assertValidUsernameAndPassword(username);
+        this.superPassword = assertValidUsernameAndPassword(password);
+        withEnv("KAFKA_SUPER_USERS", "User:" + username);
+        return this;
+    }
+
+    public AlternativeKafkaContainer withUser(final String username, final String password) {
+        usernamesAndPasswords.put(assertValidUsernameAndPassword(username), assertValidUsernameAndPassword(password));
+        return this;
     }
 
     @Override
     protected void beforeStartupPreparations() {
+        withEnv("KAFKA_LISTENER_NAME_SASL_PLAINTEXT_PLAIN_SASL_JAAS_CONFIG", createJaasLoginLine());
         uploadJaasConfig();
     }
 
     private void uploadJaasConfig() {
-        final String jaas = "KafkaServer { org.apache.kafka.common.security.plain.PlainLoginModule required "
-                            + "username=\"kafka\" password=\"kafka\" "
-                            + "user_kafka=\"kafka\" user_alice=\"alice-secret\" user_bob=\"bob-secret\"; };\n";
+        final String jaas = "KafkaServer { " + createJaasLoginLine() + " };\n";
         copyFileToContainer(Transferable.of(jaas.getBytes(StandardCharsets.UTF_8), 0644), JAAS_CONFIG_FILE);
+    }
+
+    private String createJaasLoginLine() {
+        /* Precondition: No usernames or passwords contain characters that need special handling for JAAS config. */
+        final StringBuilder sb = new StringBuilder();
+        sb.append("org.apache.kafka.common.security.plain.PlainLoginModule required username=\"");
+        sb.append(superUsername);
+        sb.append("\" password=\"");
+        sb.append(superPassword);
+        sb.append("\" user_");
+        sb.append(superUsername);
+        sb.append("=\"");
+        sb.append(superPassword);
+        sb.append("\"");
+        for (final Map.Entry<String, String> entry : usernamesAndPasswords.entrySet()) {
+            sb.append(" user_");
+            sb.append(entry.getKey());
+            sb.append("=\"");
+            sb.append(entry.getValue());
+            sb.append("\"");
+        }
+        sb.append(";");
+        return sb.toString();
+    }
+
+    private static String assertValidUsernameAndPassword(final String s) {
+        /* Enforcing, in order to not have to deal with escaping for the JAAS config. */
+        for (final char c : s.toCharArray()) {
+            if (!(c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9' || c == '-')) {
+                throw new RuntimeException("Only letters, digits and hyphens allowed in usernames and passwords.");
+            }
+        }
+        return s;
     }
 
 }
